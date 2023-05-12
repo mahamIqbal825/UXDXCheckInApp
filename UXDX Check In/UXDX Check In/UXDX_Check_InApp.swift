93e5f025556b169a86f4dd5e2495f27889445506
Rory Madden <rory.madden@sociallymotivated.com>
2023-05-12 23:14:19 +0100

    Check in all changes

---
+++ UXDX Check In/UXDX Check In/UXDX_Check_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  UXDX_Check_InApp.swift
+//  UXDX Check In
+//
+//  Created by Rory Madden on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct UXDX_Check_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
